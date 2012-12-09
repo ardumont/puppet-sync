c95db69e851c70c85dcbe8188636e110f59abc0c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-09 12:36:33 +0100

    Added the base packages module i continuously use.

--- UNIX_ROOT/etc/puppet/modules/packages/manifests/init.pp
+++ UNIX_ROOT/etc/puppet/modules/packages/manifests/init.pp
@@ -1,49 +1,60 @@
 class packages () {
+  $all = ['python-software-properties',
+          'git',
+          'gitk',
+          'tmux',
+          'wget',
+          'chromium-browser',
+          'localepurge',
+          'deborphan',
+          'language-pack-en',
+          'language-pack-fr',
+          'gparted',
+          'stalonetray',
+          'inotify-tools',
+          'ruby',
+          'curl',
+          'yajl-tools',
+          'openssh-server',
+          'sysvbanner',
+          'most',
+          'graphviz',
+          'audacious',
+          'audacious-plugins',
+          'gnuplot',
+          'latex',
+          'latex-beamer',
+          'texlive-latex-extra',
+          'texlive-fonts-recommended',
+          'ttf-marvosym',
+          'nautilus-dropbox',
+          'meld',
+          'dstat',
+          'htop',
+          'zip',
+          'unzip',
+          'rar',
+          'unrar',
+          'p7zip',
+          'p7zip-rar',
+          'xclip',
+          'gpointing-device-settings']
+
+  Exec['apt-update'] -> Package['aptitude'] -> Package[$all]
+
+  # global policy for the packages
+  Package {
+    ensure => 'installed'
+  }
+
+  exec { 'apt-update':
+    command => '/usr/bin/apt-get update'
+  }
+
+  package {'aptitude':}
 
   # fill the packages here
-  package {["git",
-            "gitk",
-            "tmux",
-            "wget",
-            "gitk",
-            "gitk",
-            "chromium-browser",
-            "localepurge",
-            "deborphan",
-            "language-pack-en",
-            "language-pack-fr",
-            "gparted",
-            "stalonetray",
-            "inotify-tools",
-            "ruby",
-            "curl",
-            "yajl-tools",
-            "openssh-server",
-            "sysvbanner",
-            "most",
-            "graphviz",
-            "audacious",
-            "audacious-plugins",
-            "gnuplot",
-            "vim",
-            "latex",
-            "latex-beamer",
-            "texlive-latex-extra",
-            "texlive-fonts-recommended",
-            "ttf-marvosym",
-            "nautilus-dropbox",
-            "gtalk",
-            "meld",
-            "dstat",
-            "htop",
-            "zip",
-            "unzip",
-            "rar",
-            "unrar",
-            "p7zip",
-            "p7zip-rar",
-            "xclip",
-            "gpointing-device-settings"]:
-    ensure => present,
+  package {$all:
+    provider => 'aptitude'
   }
 }
