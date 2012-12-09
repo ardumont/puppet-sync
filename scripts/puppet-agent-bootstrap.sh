50a3771be6acbf9f00e6febc566068e5287dd132
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-09 10:05:43 +0100

    Simplify the bootstrap agent script.

--- scripts/puppet-agent-bootstrap.sh
+++ scripts/puppet-agent-bootstrap.sh
@@ -2,19 +2,7 @@
 
 whoami
 
-#sudo /etc/init.d/network restart
-
-sudo apt-get install -y puppet facter dnsmasq bind-utils
-
-echo "nameserver 127.0.0.1" > /etc/resolv.conf
-echo "nameserver 10.0.2.3" >> /etc/resolv.conf
-
-echo "127.0.0.1               puppet localhost.localdomain localhost" > /etc/hosts
-echo "192.168.33.10           puppet.master.com" >> /etc/hosts
-echo "192.168.33.11           puppet.agent.com" >> /etc/hosts
-echo "::1             localhost6.localdomain6 localhost6" >> /etc/hosts
-
-#service dnsmasq restart
+sudo apt-get install -y puppet facter
 
 # some special setup
 grep "export TERM" /home/vagrant/.bashrc
