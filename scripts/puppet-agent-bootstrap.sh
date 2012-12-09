ebdc2ce5e6cd478c7e2a5789caca6337341af0fd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-09 11:30:02 +0100

    Add the install with the puppet labs repository.

--- scripts/puppet-agent-bootstrap.sh
+++ scripts/puppet-agent-bootstrap.sh
@@ -2,7 +2,19 @@
 
 whoami
 
-sudo apt-get install -y puppet facter
+# puppet installation once
+if [ ! -f ./puppetlabs-release-precise.deb ]; then
+    # http://docs.puppetlabs.com/guides/puppetlabs_package_repositories.html
+    wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
+    sudo dpkg -i puppetlabs-release-precise.deb
+    sudo apt-get update
+
+    # workaround
+    sudo aptitude install -y puppet facter
+
+    # stop the puppet agent service
+    sudo service puppet stop
+fi
 
 # some special setup
 grep "export TERM" /home/vagrant/.bashrc
@@ -10,4 +22,4 @@ if [ ! $? = 0 ]; then
     echo -e "\nexport TERM=xterm" >> /home/vagrant/.bashrc
 fi
 
-~/bin/puppet-agent-start-no-daemon.sh --debug --onetime
+/bin/puppet-agent-start-no-daemon.sh --debug --onetime
