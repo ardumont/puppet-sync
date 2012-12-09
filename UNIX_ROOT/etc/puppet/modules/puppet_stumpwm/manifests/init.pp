cefbdab385a8134ca83a10a2d0f8f0429c627ba6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-09 11:53:54 +0100

    Stumpwm module up and ready.

--- UNIX_ROOT/etc/puppet/modules/puppet_stumpwm/manifests/init.pp
+++ UNIX_ROOT/etc/puppet/modules/puppet_stumpwm/manifests/init.pp
@@ -1,14 +1,28 @@
 class puppet_stumpwm {
-    # install the package
-    package {'stumpwm':
-      provider => 'aptitude',
-      ensure => 'latest',
-    }
+
+   Package['stumpwm'] -> File['xsessions'] -> File['stumpwm.desktop']
+
+   $xsessions='/usr/share/xsessions'
+
+   # install the package
+   package {'stumpwm':
+     provider => 'aptitude',
+     ensure => 'latest',
+   }
+
+   # pre-requisite regarding the folder into which installed the stumpwm file
+   file { 'xsessions':
+     path => $xsessions,
+     ensure => 'directory',
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0755',
+   }
 
    # Install the file that permits to choose the window manager at login time
    file {'stumpwm.desktop':
-     path   => '/usr/share/xsessions/stumpwm.desktop',
+     path   => "$xsessions/stumpwm.desktop",
      ensure => 'present',
-     source => "puppet:///modules/puppet_stumpwm/stumpwm.desktop",
+     source => 'puppet:///modules/puppet_stumpwm/stumpwm.desktop',
    }
 }
